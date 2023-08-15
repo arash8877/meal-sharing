0816fe1e99216a5a1f5744cbed204dc356934f0f
Arash <arash8877@gmail.com>
2023-08-15 21:21:51 +0200

    added reseration form

--- src/client/components/book/ReserveForm.js
+++ src/client/components/book/ReserveForm.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './ReserveForm.css'; 
 
-const ReserveForm = () => {
+const  ReserveForm = () => {
+  const [formData, setFormData] = useState({
+    phoneNumber: '',
+    name: '',
+    email: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // You can perform further actions here, like sending the data to a server
+    console.log('Form data:', formData);
+  }
+
   return (
-    <div>ReserveForm</div>
-  )
+    <div className="form-container">
+      <h2 className='form-title'>Reserve a Seat</h2>
+      <form className="reservation-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number:</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button className="submit-button" type="submit">Reserve</button>
+      </form>
+    </div>
+  );
 }
 
-export default ReserveForm
+export default ReserveForm;
